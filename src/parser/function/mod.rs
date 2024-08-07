d466b2b59b959775c1dcf2de609a3f621b613ef0
YuYang <yyangplus@gmail.com>
2024-08-07 09:55:12 +0800

    parse parameter

--- src/parser/function/mod.rs
+++ src/parser/function/mod.rs
@@ -3,17 +3,17 @@ pub(crate) mod parse;
 
 #[derive(Debug, PartialEq)]
 pub struct Function<'a> {
-    signature: FunctionSignature<'a>,
-    body: Option<body::FunctionBody<'a>>,
+    pub signature: FunctionSignature<'a>,
+    pub body: Option<body::FunctionBody<'a>>,
 }
 
 #[derive(Debug, PartialEq)]
-pub(super) struct FunctionSignature<'a> {
-    visible: bool,
-    entry: bool,
-    return_value: Option<ReturnValue<'a>>,
-    name: &'a str,
-    parameters: Option<Parameters<'a>>,
+pub struct FunctionSignature<'a> {
+    pub visible: bool,
+    pub entry: bool,
+    pub return_value: Option<ReturnValue<'a>>,
+    pub name: &'a str,
+    pub parameters: Option<Parameters<'a>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -22,16 +22,66 @@ pub(super) struct ReturnValue<'a> {
 }
 
 #[derive(Debug, PartialEq)]
-pub(super) struct Parameters<'a> {
+pub struct Parameter<'a> {
+    pub name: &'a str,
+    pub ty: &'a str,
+    pub size: usize,
     raw_string: &'a str,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Parameters<'a> {
+    pub params: Vec<Parameter<'a>>,
+    raw_string: &'a str,
+}
+
+impl Parameters<'_> {
+    fn parse(&mut self) {
+        for p_raw_str in self.raw_string.split('\n') {
+            let p_raw_str = p_raw_str.trim();
+            let p_raw_str = p_raw_str.trim_end_matches(',');
+            let parts = p_raw_str.split(' ').collect::<Vec<_>>();
+            if parts.len() != 3 {
+                continue;
+            }
+            let ty = parts[1];
+            let name = parts[2];
+            let size = match ty {
+                ".s8" => 1,
+                ".s16" => 2,
+                ".s32" => 4,
+                ".s64" => 8,
+                ".u8" => 1,
+                ".u16" => 2,
+                ".u32" => 4,
+                ".u64" => 8,
+                ".f16" => 2,
+                ".f16x2" => 4,
+                ".f32" => 4,
+                ".f64" => 8,
+                ".b8" => 1,
+                ".b16" => 2,
+                ".b32" => 4,
+                ".b64" => 8,
+                ".b128" => 16,
+                //TODO: more types
+                _ => panic!("unknown type: {}", ty),
+            };
+            self.params.push(Parameter {
+                name: name,
+                ty: ty,
+                size: size,
+                raw_string: p_raw_str,
+            });
+        }
+    }
+}
 #[cfg(test)]
 mod test_parse_function_signature {
 
-    use crate::parser::function::parse::parse_function_signature;
+    use crate::parser::function::{parse::parse_function_signature, Parameter};
 
-    use super::{FunctionSignature, ReturnValue, Parameters};
+    use super::{FunctionSignature, Parameters, ReturnValue};
 
     #[test]
     fn visible_entry_name() {
@@ -73,8 +123,7 @@ mod test_parse_function_signature {
 
     #[test]
     fn func_no_return_trivial_parameters() {
-        let input =
-".func _ZN4core9panicking(hi)";
+        let input = ".func _ZN4core9panicking(hi)";
         let signature = parse_function_signature(input);
         assert_eq!(
             signature,
@@ -85,7 +134,10 @@ mod test_parse_function_signature {
                     entry: false,
                     return_value: None,
                     name: "_ZN4core9panicking",
-                    parameters: Some(Parameters { raw_string: "hi" }),
+                    parameters: Some(Parameters {
+                        raw_string: "hi",
+                        params: vec![],
+                    }),
                 }
             ))
         )
@@ -93,8 +145,7 @@ mod test_parse_function_signature {
 
     #[test]
     fn func_no_return_some_parameters() {
-        let input =
-".func _ZN4core9panicking
+        let input = ".func _ZN4core9panicking
 (
 	.param .b64 _ZN4core9panicking_param_0,
 	.param .b64 _ZN4core9panicking_param_1,
@@ -110,11 +161,33 @@ mod test_parse_function_signature {
                     entry: false,
                     return_value: None,
                     name: "_ZN4core9panicking",
-                    parameters: Some(Parameters { raw_string: "
+                    parameters: Some(Parameters {
+                        raw_string: "
 	.param .b64 _ZN4core9panicking_param_0,
 	.param .b64 _ZN4core9panicking_param_1,
 	.param .b64 _ZN4core9panicking_param_2
-"}),
+",
+                        params: vec![
+                            Parameter {
+                                name: "_ZN4core9panicking_param_0",
+                                ty: ".b64",
+                                size: 8,
+                                raw_string: ".param .b64 _ZN4core9panicking_param_0",
+                            },
+                            Parameter {
+                                name: "_ZN4core9panicking_param_1",
+                                ty: ".b64",
+                                size: 8,
+                                raw_string: ".param .b64 _ZN4core9panicking_param_1",
+                            },
+                            Parameter {
+                                name: "_ZN4core9panicking_param_2",
+                                ty: ".b64",
+                                size: 8,
+                                raw_string: ".param .b64 _ZN4core9panicking_param_2",
+                            },
+                        ],
+                    }),
                 }
             ))
         )
@@ -122,8 +195,7 @@ mod test_parse_function_signature {
 
     #[test]
     fn func_return_and_parameters() {
-        let input =
-".func  (.param .b64 func_retval0) _foo(
+        let input = ".func  (.param .b64 func_retval0) _foo(
 	.param .b64 _foo_param_0,
 	.param .b64 _foo_param_1
 )";
@@ -135,12 +207,30 @@ mod test_parse_function_signature {
                 FunctionSignature {
                     visible: false,
                     entry: false,
-                    return_value: Some(ReturnValue { raw_string: ".param .b64 func_retval0" }),
+                    return_value: Some(ReturnValue {
+                        raw_string: ".param .b64 func_retval0"
+                    }),
                     name: "_foo",
-                    parameters: Some(Parameters { raw_string: "
+                    parameters: Some(Parameters {
+                        raw_string: "
 	.param .b64 _foo_param_0,
 	.param .b64 _foo_param_1
-"})
+",
+                        params: vec![
+                            Parameter {
+                                name: "_foo_param_0",
+                                ty: ".b64",
+                                size: 8,
+                                raw_string: ".param .b64 _foo_param_0",
+                            },
+                            Parameter {
+                                name: "_foo_param_1",
+                                ty: ".b64",
+                                size: 8,
+                                raw_string: ".param .b64 _foo_param_1",
+                            },
+                        ]
+                    })
                 }
             ))
         )
@@ -150,17 +240,13 @@ mod test_parse_function_signature {
 #[cfg(test)]
 mod test_parse_function_body {
 
-    use crate::parser::function::{
-        parse::parse_function_body, body::FunctionBody,
-    };
+    use crate::parser::function::{body::FunctionBody, parse::parse_function_body};
 
     #[test]
     fn empty() {
         let input = ";";
         let body = parse_function_body(input);
-        assert!(
-            body.is_err()
-        )
+        assert!(body.is_err())
     }
 
     #[test]
@@ -171,7 +257,9 @@ mod test_parse_function_body {
             body,
             Ok((
                 "",
-                FunctionBody { body: Some(".reg .b32 %r<3>") }
+                FunctionBody {
+                    body: Some(".reg .b32 %r<3>")
+                }
             ))
         )
     }
@@ -179,8 +267,9 @@ mod test_parse_function_body {
 
 #[cfg(test)]
 mod test_parse_function {
-    use crate::parser::function::{parse::parse_function, Function, FunctionSignature, body::FunctionBody};
-
+    use crate::parser::function::{
+        body::FunctionBody, parse::parse_function, Function, FunctionSignature,
+    };
 
     #[test]
     fn no_return_no_parameters_no_body() {
@@ -220,7 +309,9 @@ mod test_parse_function {
                         name: "_Z6kernelPiS_i",
                         parameters: None,
                     },
-                    body: Some(FunctionBody { body: Some(" \n foo \n bar ") }),
+                    body: Some(FunctionBody {
+                        body: Some(" \n foo \n bar ")
+                    }),
                 }
             ))
         )
--- src/parser/function/parse.rs
+++ src/parser/function/parse.rs
@@ -1,81 +1,58 @@
 use nom::{
-    IResult,
-    bytes::complete::tag,
-    Parser,
-    sequence::preceded,
-    character::complete::{space0, space1, multispace0},
-    combinator::{opt, value, map},
     branch::alt,
+    bytes::complete::tag,
     character::complete::char,
+    character::complete::{multispace0, space0, space1},
+    combinator::{map, opt, value},
+    sequence::preceded,
+    IResult, Parser,
 };
 
-use crate::parser::{comment::parse::many1_comments_or_whitespace, parse_braced_balanced, parse_parenthesized_naive, parse_name};
+use crate::parser::{
+    comment::parse::many1_comments_or_whitespace, parse_braced_balanced, parse_name,
+    parse_parenthesized_naive,
+};
 
-use super::{Function, body::FunctionBody, FunctionSignature, ReturnValue, Parameters};
+use super::{body::FunctionBody, Function, FunctionSignature, Parameter, Parameters, ReturnValue};
 
 pub(crate) fn parse_function(input: &str) -> IResult<&str, Function> {
-    let (input, signature) = 
-    parse_function_signature(input)?;
+    let (input, signature) = parse_function_signature(input)?;
     let (input, body) = preceded(
         opt(many1_comments_or_whitespace),
-        alt((
-            map(
-                char(';'),
-                |_| None
-            ),
-            parse_function_body
-            .map(Some)
-        ))
+        alt((map(char(';'), |_| None), parse_function_body.map(Some))),
     )(input)?;
-    Ok((
-        input,
-        Function {
-            signature,
-            body,
-        }
-    ))
+    Ok((input, Function { signature, body }))
 }
 
 pub(super) fn parse_function_body(input: &str) -> IResult<&str, FunctionBody> {
     parse_braced_balanced
-        .map(|raw_string| FunctionBody { body: Some(raw_string) })
-    .parse(input)
+        .map(|raw_string| FunctionBody {
+            body: Some(raw_string),
+        })
+        .parse(input)
 }
 
 pub(super) fn parse_function_signature(input: &str) -> IResult<&str, FunctionSignature> {
     let (input, (visible, entry)) = alt((
-        value(
-            (true, true),
-            tag(".visible")
-            .and(space1)
-            .and(tag(".entry"))
-        ),
-        value(
-            (false, false),
-            tag(".func")
-        )
-    ))
-    (input)?;
-    
+        value((true, true), tag(".visible").and(space1).and(tag(".entry"))),
+        value((false, false), tag(".func")),
+    ))(input)?;
+
     let (input, return_value) = preceded(
         space1,
-        opt(
-            parse_parenthesized_naive
-            .map(|raw_string| ReturnValue { raw_string })
-        )
+        opt(parse_parenthesized_naive.map(|raw_string| ReturnValue { raw_string })),
     )(input)?;
 
-    let (input, name) = preceded(
-        space0,
-        parse_name
-    )(input)?;
+    let (input, name) = preceded(space0, parse_name)(input)?;
 
     let (input, parameters) = preceded(
         multispace0,
-        opt(
-            parse_parenthesized_naive
-            .map(|raw_string| Parameters { raw_string })
-        )
+        opt(parse_parenthesized_naive.map(|raw_string| -> Parameters {
+            let params: Vec<Parameter> = vec![];
+            let mut parmas = Parameters { raw_string, params };
+            parmas.parse();
+            parmas
+        })),
     )(input)?;
 
     Ok((
@@ -86,6 +63,6 @@ pub(super) fn parse_function_signature(input: &str) -> IResult<&str, FunctionSig
             return_value,
             name,
             parameters,
-        }
+        },
     ))
 }
